192dd919f1d6f244db42a19997603b669be28a59
Franck LEPRETTE <21101512+kortaone@users.noreply.github.com>
2020-09-04 11:04:37 +0000

    mongodb, mongoose, find day 4sept2020

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response, ErrorRequestHandler } from 'express';
 import morgan from "morgan";
 import helmet from "helmet";
 import { configuration, IConfig } from "./config";
+import { Profile, IProfile } from './models/profiles';
+import { connect } from './database';
+
 
 export function createExpressApp(config: IConfig): express.Express {
   const { express_debug } = config;
@@ -19,10 +22,34 @@ export function createExpressApp(config: IConfig): express.Express {
 
   app.get('/', (req: Request, res: Response) => { res.send('This is the boilerplate for Flint Messenger app') });
 
+  app.post('/profile' ,  ( req: Request , res: Response) => {
+  const { email, firstname, lastname} = req.body ;
+  const newProfile= new Profile({email:email, firstname:firstname, lastname:lastname});
+  newProfile.save();
+
+  //curl http://10.0.3.140:3000/profile -X POST --data '{"email":"franck.leprette@gmail.com","firstname":"Franck","lastname":"LEPRETTE"}' -H'Content-type:application/json'
+  res.send("User created");
+
+  });
+
+
+  app.get('/profile/:profileId' ,  ( req: Request , res: Response) => {
+    const profileId = req.params['profileId'];
+   Profile.findById(profileId, '_id email', (err, profile) => { 
+      if(err) { console.log("Error found");}
+      if(profile == null) { res.status(404); return;}
+      res.send(profile.email)
+    });
+    
+  });
+
+
   return app;
 }
 
 const config = configuration();
 const { PORT } = config;
 const app = createExpressApp(config);
-app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`));
+connect(config).then(
+  () => { app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`)) }
+);
